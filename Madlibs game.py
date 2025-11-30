f0d215c32cabe7d83604194ee9204587f5c6ff07
hafeelfaleel <hafeelfaleel@gmail.com>
2025-11-30 19:02:04 +0530

    Madlibs game BroCode

---
+++ Madlibs game.py
@@ -0,0 +1,14 @@
+# MADLIBS GAME
+# word game where you create a story
+# by filling in blanks with random words
+
+adjective = input("Enter an adjective (description): ")
+noun1 =  input("Enter the noun (person, place, thing): ")
+adjective2 = input("Enter an adjective(description): ")
+verb1 =  input("Enter the verb ending with 'ing': ")
+adjective3 = input("Enter an adjective (description): ")
+
+print(f"Today I went to a {adjective} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
