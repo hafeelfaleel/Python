d7c68db4632ca14e560151bcfadec4a8bb3c570d
hafeelfaleel <hafeelfaleel@gmail.com>
2025-11-30 19:33:25 +0530

    BroCode

---
+++ Area of a Circle.py
@@ -0,0 +1,6 @@
+import math
+radius = float(input("Enter the radius of a circle: "))
+area = math.pi * radius ** 2
+print(f"The area of the circle is: {round(area, 2)}cm²")
+
+# numlock + alt + 0178 to print the power of 2 like above " cm²"
